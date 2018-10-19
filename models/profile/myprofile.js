9c047c66a39313670f9951f5e2160f9cbf694ee6
Shamal <ssdh.gdrive@gmail.com>
2018-10-19 15:41:23 +0530

    Added SQL to gitignore file. Added the myprofile.js to new models/profile folder to create models for profile summaries for my profile access level. Route / in index.js was included a conditional statement to save resources for users with low access levels and to load day summary using /models/profile/myprofile.js. Displayed day profile summary for users with low access levels.
    Signed-off-by: Shamal <ssdh.gdrive@gmail.com>

---
+++ models/profile/myprofile.js
@@ -0,0 +1,18 @@
+const MySql = require('../comms/mySqlCon')
+const ProfileMyProfile = module.exports = {}
+
+ProfileMyProfile.profile = function(username, start_date, end_date, callback) {
+    MySql.pool.getConnection(function(pool_err, connection) {
+        if(pool_err) {
+            return callback(pool_err, null)
+        }
+
+        connection.query('CALL OfficerProfile(?, ?, ?);', [username, start_date, end_date], function(err, rows, fields) {
+            connection.release()
+            if(err) {
+                return callback(err, null)
+            }
+            callback(err, rows)
+        })
+    })
+}
