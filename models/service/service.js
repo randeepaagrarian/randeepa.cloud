df1de80fefcbe5a1b04987f648c440a19617876a
Shamal Sandeep <ssdh.gdrive@gmail.com>
2019-06-16 13:08:48 +0530

    Basic service functions complete
    Added new service, service list by date, allocate technician, change technician

--- models/service/service.js
+++ models/service/service.js
@@ -16,3 +16,124 @@ Service.getTechnicians = function(callback) {
         })
     })
 }
+
+Service.newService = function(service, callback) {
+    MySql.pool.getConnection(function(pool_err, connection) {
+        if(pool_err) {
+            return callback(pool_err, null)
+        }
+        connection.query('INSERT INTO service SET ?', service, function(err, result) {
+            connection.release()
+            if(err) {
+                return callback(err, false)
+            }
+            callback(err, true)
+        })
+    })
+}
+
+Service.byDate = function(startDate, endDate, callback) {
+    MySql.pool.getConnection(function(pool_err, connection) {
+        if(pool_err){
+            return callback(pool_err, null)
+        }
+        connection.query('SELECT SR.id, SR.date, SR.issue, SR.sale_id, SR.sale_date, S.sys_date as s_sale_date, SR.chassis_no, S.chassis_no as s_chassis_no, SR_M.name as model_name, S_M.name as s_model_name, SR.meter, SR.meter_type, SR.customer_name, SR. customer_contact, S.customer_contact as s_customer_contact, SR.current_address, S.customer_address as s_customer_address, SR_D.name as dealer_name, S_D.name as s_dealer_name, U.name as technician_name, SR.work_sheet, SR.technician_allocated, SR.service_completed FROM service SR LEFT JOIN sale S ON S.id = SR.sale_id LEFT JOIN dealer SR_D ON SR.dealer_id = SR_D.id LEFT JOIN dealer S_D ON S.location_fk = S_D.id LEFT JOIN model SR_M ON SR.model_id = SR_M.id LEFT JOIN model S_M on S.model = S_M.id LEFT JOIN user U ON SR.technician_id = U.username WHERE DATE(SR.date) BETWEEN ? AND ?;', [startDate, endDate], function(err, rows, fields) {
+            connection.release()
+            if(err) {
+                return callback(err, null)
+            }
+            callback(err, rows)
+        })
+    })
+}
+
+Service.saleIDEntered = function(serviceID, callback) {
+    MySql.pool.getConnection(function(pool_err, connection) {
+        if(pool_err) {
+            return callback(pool_err, null)
+        }
+        connection.query('SELECT sale_id FROM service WHERE id = ?', serviceID, function(err, rows, fields) {
+            connection.release()
+            if(err) {
+                return callback(err, false)
+            }
+
+            callback(err, rows)
+        })
+    })
+}
+
+Service.serviceInfo = function(serviceID, callback) {
+    MySql.pool.getConnection(function(pool_err, connection) {
+        if(pool_err){
+            return callback(pool_err, null)
+        }
+        connection.query('SELECT SR.id, SR.date, SR.issue, SR.sale_id, SR.sale_date, SR.chassis_no, SR_M.name as model_name, SR.meter, SR.meter_type, SR.customer_name, SR. customer_contact, SR.current_address, SR_D.name as dealer_name, U.name as technician_name, SR.work_sheet, SR.technician_allocated, SR.technician_allocated_by, SR.technician_allocated_on, SR.service_completed, SR.service_completed_remarks, SR.service_completed_by, SR.service_completed_on FROM service SR LEFT JOIN dealer SR_D ON SR.dealer_id = SR_D.id LEFT JOIN model SR_M ON SR.model_id = SR_M.id LEFT JOIN user U ON SR.technician_id = U.username WHERE SR.id = ?;', [serviceID], function(err, rows, fields) {
+            connection.release()
+            if(err) {
+                return callback(err, null)
+            }
+            callback(err, rows)
+        })
+    })
+}
+
+Service.allocateTechnician = function(technician, serviceID, callback) {
+    MySql.pool.getConnection(function(pool_err, connection) {
+        if(pool_err) {
+            return callback(pool_err, null)
+        }
+        connection.query('UPDATE service SET ? WHERE id = ?', [technician, serviceID], function(err, result) {
+            connection.release()
+            if(err) {
+                return callback(err, false)
+            }
+            callback(err, true)
+        })
+    })
+}
+
+Service.changeTechnician = function(serviceID, datetime, username, reason, technician, callback) {
+    MySql.pool.getConnection(function(pool_err, connection) {
+        if(pool_err) {
+            return callback(pool_err, null)
+        }
+
+        connection.beginTransaction(function(err) {
+            if(err) {
+                connection.release()
+                return callback(err, false)
+            }
+
+            connection.query('INSERT INTO service_technician_change_history (date_in, service_id, changed_by, technician_id, technician_allocated_by, technician_allocated_on, reason) SELECT ?, id, ?, technician_id, technician_allocated_by, technician_allocated_on, ? FROM service WHERE id = ?', [datetime, username, reason, serviceID], function(err, result) {
+                if(err) {
+                    return connection.rollback(function() {
+                        connection.release()
+                        callback(err, false)
+                    })
+                }
+
+                connection.query('UPDATE service SET ? WHERE id = ?', [technician, serviceID], function(err, result) {
+                    if(err) {
+                        return connection.rollback(function() {
+                            connection.release()
+                            callback(err, false)
+                        })
+                    }
+
+                    connection.commit(function(err) {
+                        if(err) {
+                            return connection.rollback(function() {
+                                connection.release()
+                                callback(err, false)
+                            })
+                        }
+                        connection.release()
+                        callback(err, true)
+                    })
+                })
+            })
+
+        })
+    })
+}
--- routes/service/service.js
+++ routes/service/service.js
@@ -7,13 +7,184 @@ const Auth = require('../../functions/auth')
 const MDate = require('../../functions/mdate')
 
 const Service = require('../../models/service/service')
+const Stock = require('../../models/stock/stock')
+const Sale = require('../../models/sale/sale')
 
 router.use(Auth.signedIn, Auth.validServiceUser, function(req, res, next) {
     next()
 })
 
-router.get('/hello', function(req, res) {
+router.get('/new', function(req, res) {
+    async.series([
+        function(callback) {
+            Stock.getModels(callback)
+        }, function(callback) {
+            Stock.getDealersAndShowrooms(callback)
+        }
+    ], function(err, data) {
+        res.render('service/new', {
+            title: 'New Service',
+            navbar: 'Service',
+            user: req.user,
+            models: data[0],
+            dealersAndShowrooms: data[1]
+        })
+    })
+})
 
+router.post('/new', function(req, res) {
+
+    if(req.body.issue == '') {
+        req.flash('warning_msg', 'Please enter the issue')
+        res.redirect('/service/new')
+    }
+
+    let service = {}
+
+    for(let key in req.body) {
+        if(req.body[key] == '' || req.body[key] == -1)
+            continue
+        service[key] = req.body[key]
+    }
+
+    service['date'] = MDate.getDateTime()
+
+    async.series([
+        function(callback) {
+            Service.newService(service, callback)
+        }
+    ], function(err, data) {
+        if(data[0] == true) {
+            req.flash('warning_msg', 'Service successfully opened')
+            res.redirect('/service/new')
+        } else {
+            req.flash('warning_msg', 'Failed to open service')
+            res.redirect('/service/new')
+        }
+    })
+
+})
+
+router.get('/date', function(req, res) {
+    async.series([
+        function(callback) {
+            Service.byDate(req.query.startDate, req.query.endDate, callback)
+        }
+    ], function(err, data) {
+        res.render('service/bydate', {
+            title: 'Services By Date',
+            navbar: 'Service',
+            user: req.user,
+            services: data[0],
+            results: data[0].length
+        })
+    })
+})
+
+router.get('/serviceInfo', function(req, res){
+    async.series([
+        function(callback) {
+            Service.saleIDEntered(req.query.serviceID, callback)
+        }
+    ], function(err, data) {
+
+        let saleID = data[0][0].sale_id
+
+        if(saleID != null) {
+            async.series([
+                function(callback) {
+                    Service.serviceInfo(req.query.serviceID, callback)
+                }, function(callback){
+                    Service.getTechnicians(callback)
+                }, function(callback) {
+                    Sale.cloudIDInfo(saleID, callback)
+                }
+            ], function(err, data) {
+                res.render('service/serviceInfo', {
+                    title: 'Service Info',
+                    navbar: 'Service',
+                    user: req.user,
+                    serviceInfo: data[0],
+                    saleIDEntered: true,
+                    technicians: data[1],
+                    sales: data[2]
+                })
+            })
+        } else {
+            async.series([
+                function(callback) {
+                    Service.serviceInfo(req.query.serviceID, callback)
+                }, function(callback){
+                    Service.getTechnicians(callback)
+                }
+            ], function(err, data) {
+                res.render('service/serviceInfo', {
+                    title: 'Service Info',
+                    navbar: 'Service',
+                    user: req.user,
+                    serviceInfo: data[0],
+                    saleIDEntered: false,
+                    technicians: data[1],
+                })
+            })
+        }
+    })
+})
+
+router.post('/allocateTechnician/:serviceID', function(req, res) {
+    let technician = {
+        technician_id: req.body.technician,
+        technician_allocated: 1,
+        technician_allocated_by: req.user.username,
+        technician_allocated_on: MDate.getDateTime()
+    }
+
+    async.series([
+        function(callback) {
+            Service.allocateTechnician(technician, req.params.serviceID, callback)
+        }
+    ], function(err, data) {
+        if(data[0] == true) {
+            req.flash('success_msg', 'Technician allocated')
+            res.redirect('/service/serviceInfo?serviceID=' + req.params.serviceID)
+        } else {
+            req.flash('warning_msg', 'Failed to allocated technician')
+            res.redirect('/service/serviceInfo?serviceID=' + req.params.serviceID)
+        }
+    })
+})
+
+router.post('/changeTechnician/:serviceID', function(req, res) {
+
+    if(req.body.reason == '') {
+        req.flash('warning_msg', 'Please specify the reason for technician change')
+        res.redirect('/service/serviceInfo?serviceID=' + req.params.serviceID)
+        return
+    }
+
+    let timeNow = MDate.getDateTime()
+
+    let technician = {
+        technician_id: req.body.technician,
+        technician_allocated_by: req.user.username,
+        technician_allocated_on: timeNow
+    }
+
+    async.series([
+        function(callback) {
+            Service.changeTechnician(req.params.serviceID, timeNow, req.user.username, req.body.reason, technician, callback)
+        }
+    ], function(err, data) {
+        if(data[0] == true) {
+            req.flash('success_msg', 'Technician successfully changed')
+            res.redirect('/service/serviceInfo?serviceID=' + req.params.serviceID)
+            return
+        } else {
+            req.flash('warning_msg', 'Failed to change technician')
+            res.redirect('/service/serviceInfo?serviceID=' + req.params.serviceID)
+            return
+        }
+    })
 })
 
 module.exports = router
